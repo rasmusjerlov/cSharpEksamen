363c6d525113180e65987a80d741d6d2ef3b92f7
rasmusjerlov <rasmus.jerloev@gmail.com>
2024-12-02 10:10:39 +0100

    Project start

---
+++ cSharpEksamen/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cSharpEksamen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
